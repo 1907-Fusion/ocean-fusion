e0bb28c6107a17da43a2704e27db641986420f20
Martha Ivana <martha.syryca@gmail.com>
2019-10-07 14:54:05 -0400

    feat(question-display): question and choices appear, no styling yet

--- client/components/game.js
+++ client/components/game.js
@@ -2,12 +2,18 @@
 import React from 'react'
 import Loading from './loading'
 import Camera from './camera'
+import Question from './question'
 
 class Game extends React.Component {
   render() {
     return (
-      <div>
-        <Camera />
+      <div id="main">
+        <div id="main-left">
+          <Camera />
+        </div>
+        <div id="main-right">
+          <Question />
+        </div>
       </div>
     )
   }
