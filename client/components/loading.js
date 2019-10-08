3736fea534b60f6d7768942a7a8ff473811dd0e9
Martha Ivana <martha.syryca@gmail.com>
2019-10-08 17:21:57 -0400

    feat(filter): color filter adjusted, removed bubbles for performance

--- client/components/loading.js
+++ client/components/loading.js
@@ -4,7 +4,7 @@ class Loading extends React.Component {
   render() {
     return (
       <div className="loading">
-        <h1 className="loadingEarth">Saving the Earth...</h1>
+        <h1>Saving the Earth...</h1>
         <img
           id="earth"
           src="http://sannenijboer.nl/wp-content/uploads/2018/01/wereldbol.png"
