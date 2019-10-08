1f10514c2671104427724ff6de6b01124d31f01c
Amy Lee <lee.asl@gmail.com>
2019-10-08 17:23:33 -0400

    feat(animation): added animation

---
+++ client/components/fish.js
@@ -0,0 +1,81 @@
+import React from 'react'
+
+class Fish extends React.Component {
+  render() {
+    return (
+      <div className="animationContainer">
+        <div className="sun" />
+        <div className="clouds">
+          <div />
+          <div />
+          <div />
+        </div>
+        <div className="birds" />
+        <div className="sea">
+          <div className="wave w-1" />
+          <div className="wave w-2" />
+          <div className="fish">
+            <span />
+            <span />
+            <span />
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="grass">
+            <span />
+            <span />
+            <span> </span>
+          </div>
+          <div className="grass">
+            <span> </span>
+            <span> </span>
+            <span> </span>
+          </div>
+          <div className="grass">
+            <span> </span>
+            <span> </span>
+            <span> </span>
+          </div>
+          <div className="grass">
+            <span> </span>
+            <span> </span>
+            <span> </span>
+          </div>
+          <div className="circle">
+            <span />
+            <span />
+            <span />
+            <span />
+            <span />
+          </div>
+          <div className="circle">
+            <span />
+            <span />
+            <span />
+            <span />
+            <span />
+          </div>
+          <div className="circle">
+            <span />
+            <span />
+            <span />
+            <span />
+            <span />
+          </div>
+          <div className="circle">
+            <span />
+            <span />
+            <span />
+            <span />
+            <span />
+          </div>
+          <div className="grass_tw" />
+          <div className="grass_tw" />
+          <div className="grass_tw" />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Fish
