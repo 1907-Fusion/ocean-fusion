1c37e6eaa70eef254780042dcad6696fc037fc63
martha-ivana <52048527+martha-ivana@users.noreply.github.com>
2019-10-13 09:45:08 -0400

    Update users.spec.js

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -9,29 +9,6 @@ describe('User routes', () => {
     return db.sync({force: true})
   })
 
-  // describe('/api/users/', () => {
-  //   it('redirects the user to home page for logged in users', async () => {
-  //     const codysEmail = 'cody@puppybook.com'
-  //     const codysPassword = 'password'
-  //     User.create({
-  //       email: codysEmail,
-  //       password: codysPassword
-  //     })
-  //     const postData = {
-  //       email: codysEmail,
-  //       password: codysPassword
-  //     }
-  //     const loggedinUser = await request(app)
-  //       .post('/auth/login')
-  //       .type('form') //browsers have form
-  //       .send(postData) // log in cody
-  //     const cookie = loggedinUser.headers['set-cookie'] //we need to mimic a successfull login when we run the test we actually don`t use a browser so I mimic the login cookie.
-  //     await request(app)
-  //       .get('/api/users')
-  //       .set('cookie', cookie) // when we use browser it explicitly done otomatically.
-  //       .expect(302)
-  //   })
-  // })
   it('shows all users for logged in admin users', async () => {
     const codysEmail = 'cody@puppybook.com'
     const codysPassword = 'password'
